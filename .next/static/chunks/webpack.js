9843f81fa4f1f65ac419839be7b901fa6281a3a4
Flavio Silva <contatoflaviodev@gmail.com>
2021-02-23 14:37:36 -0300

    Adicionando o componente Profile

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "12c814f6450412657da2";
+/******/ 	var hotCurrentHash = "06edce0ee22d6512ece8";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
