bb51474641c841202e390b1377c1b956eff4979a
Flavio Silva <contatoflaviodev@gmail.com>
2021-02-23 15:37:11 -0300

    Adicionando componente CompletedChallenge

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "06edce0ee22d6512ece8";
+/******/ 	var hotCurrentHash = "1b58d583d284c865d259";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
